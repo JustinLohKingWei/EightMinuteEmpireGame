9d355bc6a96daea28de40034b09c2f28c3f4211c
Etienne Pham Do <62301839+majockster@users.noreply.github.com>
2021-02-08 14:30:15 -0500

    Add files via upload

---
+++ Player/Player.cpp
@@ -0,0 +1,76 @@
+#ifndef PLAYER_CPP
+#define PLAYER_CPP
+
+#include "Player.h";
+#include <iostream>
+using namespace std;
+
+	Player::Player() {
+		
+	}
+	/*
+	* Player::Player(vector<Territory>* listOfTerritories, Hand* aHand, BidingFacility* aBidingFacility){
+	*	listOfTerritories = &listOfTerritories;
+	*	myHand = &aHand;
+	*	myBidingFacility = &aBidingFacility;
+	* }
+	* 
+	* Player::Player(Player &b){
+	*	listOfTerritories = b.listOfTerritories;
+	*	myHand = b.myHand;
+	*	myBidingFacility = b.myBidingFacility;
+	* }
+	*/
+	
+	//Add assignment operator and stream insertion operator
+	void Player::PayCoin() {
+		cout << " executing PayCoin()..."<<endl;
+	}
+
+	void Player::PlaceNewArmies() {
+		cout << " executing PlaceNewArmies()..." << endl;
+	}
+
+	void Player::MoveArmies() {
+		cout << " executing MoveArmies()..." << endl;
+	}
+
+	void Player::MoveOverLand() {
+		cout << " executing MoveOverLand()..." << endl;
+	}
+
+	void Player::BuildCity() {
+		cout << " executing BuildCity()..." << endl;
+	}
+
+	void Player::DestroyArmy() {
+		cout << " executing DestroyArmy()..." << endl;
+	}
+
+	/*
+	* void Player::setHand(Hand* aHand){
+	*	myHand = &aHand;
+	* }
+	* 
+	* void Player::setBidingFacility(BidingFacility* aBidingFacilty){
+	*	myBidingFacility = &aBidingFacilty;
+	* }
+	* 
+	* void Player::setListOfTerritories(vector<Territory>* list){
+	*	listOfTerritories = &list;
+	* }
+	* 
+	* Hand Player::getHand(){
+	*	return myHand;//A pointer or raw value or a copy?
+	* }
+	* 
+	* BidingFacilty Player::getBidingFacility(){
+	*	return myBidingFacility;
+	* }
+	* 
+	* vector<Territory> Player::getListOfTerritories(){
+	*	return listOfTerritories;
+	*}
+	*/
+	 
+#endif
---
+++ Player/Player.h
@@ -0,0 +1,28 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <vector>
+
+class Player {
+public:
+	Player();
+	//Player(vector<Territory>* listOfTerritories, Hand* aHand, BidingFacility* aBidingFacility);
+	//Player(Player &b);
+	void PayCoin();
+	void PlaceNewArmies();
+	void MoveArmies();
+	void MoveOverLand();
+	void BuildCity();
+	void DestroyArmy();
+
+	//void setHand(Hand* aHand);
+	//void setBidingFacility(BidingFacility* aBidingFacility);
+	//void setListOfTerritories(vector<Territory>* list);
+	//Hand getHand();
+	//BidingFacility getBidingFacility();
+	//vector<Territory> getListOfTerritories();
+private:
+	//vector<Territory> listOfTerritories;
+	//Hand myHand;
+	//BidingFacility myBidingFacility;
+};
+#endif
