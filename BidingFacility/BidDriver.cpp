40fb47d805e8acaef4193e44a2b58d1f9c59bfa0
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-02-16 20:09:00 -0500

    added bid header

---
+++ BidingFacility/BidDriver.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Bid.h"
+#include "../Player/Player.h"
+// using namespace std;
+
+int main()
+{
+    vector<string> test{"John", "Zee", "Harry", "Bob", "Babk", "Bac"};
+    vector<Player> players;
+    // Player *p = new Player();
+
+    Bid *b = new Bid();
+    // Bid b(players);
+    // std ::cout << " Biggest name is :" << b.findLastNameOrder(b.getPlayers()) << endl;
+    return 0;
+}
