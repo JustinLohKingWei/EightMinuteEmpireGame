7a5e4c09e6a5192e97efd0fc8ee8401c85e974de
johndwight <j_paraga@live.concordia.ca>
2021-03-23 16:24:05 -0400

    Part5

---
+++ Game/Game.h
@@ -0,0 +1,12 @@
+/*
+Contains the different components that have to do with the
+progression of a game
+*/
+
+
+#pragma once
+
+// Represents the part of the board where a player picks a card
+class FaceUpCards {
+
+};
---
+++ Part5.h
@@ -0,0 +1,18 @@
+/*
+Main game loop: after the action
+After a player takes their cards and action 
+- That is, sliding the ramining cards to the LEFT to
+  fill in the empty card space
+	- then, draw a new card and place it in the RIGHT-MOST space
+Passes to the second player
+
+RQ's:
+- Sliding of the remaining cards to the left to fill in the empty card space
+- Draw of a new card and place it in the right-most space
+- Play passes to the next player
+*/
+
+#pragma once
+
+
+
