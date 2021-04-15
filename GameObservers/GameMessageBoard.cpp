938d95c8a35a1a10d71202ff804507e122db5c87
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-04-15 00:48:04 -0400

    Added and completed observer classes

--- GameObservers/GameMessageBoard.cpp
+++ GameObservers/GameMessageBoard.cpp
@@ -9,8 +9,27 @@ GameMessageBoard::GameMessageBoard(Player *p)
   //Upon instantiation, attaches itself to a ClockTimer
   _subject = p;
   _subject->Attach(this);
-}
+};
 
-void Update() {}
-void Display() {}
-void SetSubject(Player *p) {}
+GameMessageBoard::GameMessageBoard()
+{
+  _subject->Detach(this);
+};
+
+void GameMessageBoard::Update()
+{
+  Display();
+};
+void GameMessageBoard::Display(){
+    //TODO DISPLAY STATS HERE
+
+    //int stat1 = _subject.getStat1();
+
+  //cout<<stat1;
+
+};
+
+void GameMessageBoard::SetSubject(Player *p)
+{
+  _subject = p;
+};
--- GameObservers/GameMessageBoard.h
+++ GameObservers/GameMessageBoard.h
@@ -1,18 +1,18 @@
 #pragma once
-#include"./Player/Player.h";
-#include"GameObservers.h";
+#include "./Player/Player.h";
+#include "GameObservers.h";
 
-class GameMessageBoard : public GameObservers           //concrete observers
+class GameMessageBoard : public GameObservers //concrete observers
 {
 
 public:
     GameMessageBoard();
-    GameMessageBoard(Player* p);
+    GameMessageBoard(Player *p);
     ~GameMessageBoard();
     void Update();
     void Display();
-    void SetSubject(Player* p);
-private:
-    Player* _subject;
+    void SetSubject(Player *);
 
+private:
+    Player *_subject;
 };
